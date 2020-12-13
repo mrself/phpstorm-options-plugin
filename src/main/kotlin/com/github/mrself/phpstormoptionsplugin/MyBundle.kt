15473db784d737465ecbc321d9fad8d4573d7001
GitHub Action <action@github.com>
2020-12-13 19:29:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/mrself/phpstormoptionsplugin/MyBundle.kt
+++ src/main/kotlin/com/github/mrself/phpstormoptionsplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mrself.phpstormoptionsplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/mrself/phpstormoptionsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mrself.phpstormoptionsplugin.services
+
+import com.github.mrself.phpstormoptionsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
